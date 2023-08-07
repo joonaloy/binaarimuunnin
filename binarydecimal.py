e01db614759750da974dffb36d13c2199a43beae
joona <118251072+joonaloy@users.noreply.github.com>
2023-08-07 13:08:24 +0300

    Update binarydecimal.py

--- binarydecimal.py
+++ binarydecimal.py
@@ -1,22 +1,16 @@
 numero=str(input("anna binääri numero: ")) #01000101 = 69 = e
 numeroy=1
 numeroyl=[]
-#(len(numero))
-#(numero[1])
 laskuri=1
 while laskuri<len(numero):
-    #print(laskuri)
     numeroy*=2
     numeroyl.append(2**(len(numero)-laskuri))
     laskuri+=1
 numeroyl.append(1)
-#print(numeroyl)
-#print(numeroy)
 laskuri=0
 lasku=[]
 while laskuri<len(numero):
     if numero[laskuri]=="1":
-        #print(numero[laskuri])
         lasku.append(numeroyl[laskuri])
     laskuri+=1
 final=0
